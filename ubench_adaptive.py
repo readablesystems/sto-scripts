902f4d77278c6328c67e9129b08f3448d177a59b
Yihe Huang <yihehuang@g.harvard.edu>
2017-11-13 17:58:06 -0500

    adaptive rwlock microbenchmark scripts

---
+++ ubench_adaptive.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import subprocess,json,os,optparse
+from sto import profile_parser as parser
+
+TYPE = 'ubench'
+NAME = 'adaptive'
+
+DRY_RUN = None
+RESULT_DIR = 'results/json/'
+RESULT_FILE = RESULT_DIR + '{}_{}_results.json'.format(TYPE, NAME)
+
+ntrails = 5
+
+threads = [4,8,12,16,20,24]
+systems = ['array-adaptive', 'array-nonopaque', 'swissarray']
+levels = ['med', 'high', 'low']
+
+skew_vals = {'low':0.0, 'med':0.8, 'high':1.0}
+
+def cmd_opts(sys, skew, thrs):
+    opt = './concurrent 4 {0} --opspertrans=20 --ntrans=5000000 --nthreads={1} --skew={2} --readonlypercent=1.0'
+    return opt.format(sys, thrs, skew)
+
+def run_single(sys, skew, thrs):
+    global DRY_RUN
+
+    cmd = cmd_opts(sys, skew, thrs)
+    print cmd
+
+    if DRY_RUN:
+        return (0,0,0)
+
+    out = subprocess.check_output(cmd.split(' '), stderr=subprocess.STDOUT)
+
+    time = parser.extract('time', out)
+    commits = parser.extract('commits', out)
+    aborts = parser.extract('aborts', out)
+    cabrts = parser.extract('commit_time_aborts', out)
+
+    xput = commits/time
+
+    return (xput,aborts,cabrts)
+
+def key(system_name, workload, nthreads, trail):
+    return '{}/{}/{}/{}'.format(system_name,workload,nthreads,trail)
+
+def run_all(results):
+    global DRY_RUN
+
+    for sys in systems:
+        for cont in levels:
+            for tr in threads:
+                for n in range(ntrails):
+                    k = key(sys,cont,tr,n)
+                    if k in results:
+                        continue
+                    res = run_single(sys, skew_vals[cont], tr)
+                    if DRY_RUN:
+                        continue
+                    results[k] = res
+    return results
+
+if __name__ == '__main__':
+    psr = optparse.OptionParser()
+    psr.add_option('-f', action="store_true", dest="force_update", default=False)
+    psr.add_option('-d', action="store_true", dest="dry_run", default=False)
+    opts, args = psr.parse_args()
+    DRY_RUN = opts.dry_run
+
+    old_results = None
+    if os.path.exists(RESULT_FILE) and not opts.force_update:
+        with open(RESULT_FILE, 'r') as rf:
+            old_results = json.load(rf)
+    else:
+        old_results = {}
+
+    r = run_all(old_results)
+    print 'ALL DONE'
+
+    if DRY_RUN:
+        exit()
+
+    with open(RESULT_FILE, 'w') as ofile:
+        json.dump(r,ofile,indent=4,sort_keys=True)
---
+++ ubench_adaptive_graph.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import json,plot_helper
+import ubench_adaptive as exp
+import bench_color_map as cm
+import numpy as np
+from matplotlib import pyplot as plt
+
+g_threads = exp.threads
+g_systems = exp.systems
+
+def invariant(condition, msg):
+    if not condition:
+        print msg
+        exit()
+
+def g_key(sys_name, workload, nthreads):
+    return '/'.join((sys_name, workload, str(nthreads)))
+
+def process(results):
+    processed_exp = {}
+
+    for sys in exp.systems:
+        for cont in exp.levels:
+            for tr in g_threads:
+                xput_series = []
+                abrts_series = []
+                cabrts_series = []
+                for n in range(exp.ntrails):
+                    k = exp.key(sys,cont,tr,n)
+                    xput,abrts,cabrts = results[k]
+
+                    xput_series.append(xput)
+                    abrts_series.append(abrts)
+                    cabrts_series.append(cabrts)
+
+                xput_med = np.median(xput_series)
+                xput_min = np.amin(xput_series)
+                xput_max = np.amax(xput_series)
+
+                med_idx = xput_series.index(xput_med)
+
+                rec = [[xput_min, xput_med, xput_max],
+                       abrts_series[med_idx],
+                       cabrts_series[med_idx]]
+
+                processed_exp[g_key(sys,cont,tr)] = rec
+
+    return processed_exp
+
+graph_info_template = {
+    'graph_title': 'TArray throughput, {} contention',
+    'x_label': '# threads',
+    'y_label': 'Throughput (Mtxns/sec)',
+    'series_names': ('TAdaptive', 'TNonopaque', 'TSwiss'),
+    'plot_colors': ('red', 'blue', 'green'),
+    'plot_shapes': ('-^', '-o', '-v'),
+    'legends_on': True,
+    'save_name': 'ubench_adaptive_{}.pdf'
+}
+
+def pack_plotting_data(processed_results, cont):
+    meta = graph_info_template.copy()
+    common_x = g_threads
+    y_serieses = []
+    y_errors = []
+    for sys in g_systems:
+        series_data = []
+        series_error_down = []
+        series_error_up = []
+        for tr in g_threads:
+            res = processed_results[g_key(sys,cont,tr)]
+            xput = res[0]
+            series_data.append(xput[1]/1000000.0)
+            series_error_down.append((xput[1]-xput[0])/1000000.0)
+            series_error_up.append((xput[2]-xput[1])/1000000.0)
+        y_serieses.append(series_data)
+        y_errors.append((series_error_down, series_error_up))
+    meta['graph_title'] = meta['graph_title'].format(cont)
+    meta['save_name'] = meta['save_name'].format(cont)
+
+    return (meta, common_x, y_serieses, y_errors)
+
+def draw_line(meta_info, common_x, y_serieses, y_errors):
+    # Sanity checks
+    for y in y_serieses:
+        invariant(len(y) == len(common_x), "Data dimension error!")
+    for y in y_errors:
+        for yy in y:
+            invariant(len(yy) == len(common_x), "Data dimension error!")
+    invariant(len(y_serieses) == len(y_errors), "Data dimension error!")
+
+    fig, ax = plt.subplots(figsize=(7,4))
+
+    N = len(y_serieses)
+    lines = []
+
+    for i in range(0,N):
+        y = y_serieses[i]
+        y_err_pair = y_errors[i]
+        lines.append(ax.errorbar(common_x, y,
+                color=meta_info['plot_colors'][i],
+                yerr=y_err_pair, fmt=meta_info['plot_shapes'][i]))
+
+    ax.set_ylabel(meta_info['y_label'])
+    ax.set_xticks(common_x)
+    ax.set_xlabel(meta_info['x_label'])
+
+    if meta_info['legends_on']:
+        ax.legend([l[0] for l in lines],
+                  [meta_info['series_names'][i] for i in range(N)],
+                  loc='best')
+
+    plt.show()
+    #plt.savefig(meta_info['save_name'])
+
+if __name__ == '__main__':
+    with open(exp.RESULT_FILE, 'r') as rf:
+        results = json.load(rf)
+    processed_results = process(results)
+    data = pack_plotting_data(processed_results, 'med')
+    draw_line(data[0], data[1], data[2], data[3])
