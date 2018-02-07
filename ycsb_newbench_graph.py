d87058bea1f4401d45d1ca623b014156dc852d5b
Yihe Huang <yihehuang@g.harvard.edu>
2018-02-06 20:06:06 -0500

    new graph schemes

--- ycsb_newbench_graph.py
+++ ycsb_newbench_graph.py
@@ -6,7 +6,8 @@ import bench_color_map as cm
 import numpy as np
 from matplotlib import pyplot as plt
 
-g_threads = exp.threads
+#g_threads = exp.threads
+g_threads = [8,16,32,64,128]
 g_systems = exp.systems
 #g_systems = ['default', 'tictoc']
 
@@ -49,7 +50,9 @@ graph_info_template = {
     'x_label': '# threads',
     'y_label': 'Throughput (Mtxns/sec)',
     'series_names': ('OCC', 'SwissTM', 'Adaptive', '2PL', 'TicToc'),
-    'plot_colors': ('blue', 'green', 'red', 'purple', 'grey'),
+    'fill_colors': ('black', 'white', 'white', 'white', 'white'),
+    #'edge_colors': ('blue', 'green', 'red', 'purple', 'grey'),
+    'hatches': ('', '++', '///', 'xx', ''),
     #'series_names': ('OCC', 'TicToc'),
     #'plot_colors': ('blue', 'grey'),
     'legends_on': True,
@@ -59,6 +62,8 @@ graph_info_template = {
 def pack_plotting_data(processed_results, cont):
     print "xput:"
     meta = graph_info_template.copy()
+    if exp.NAME == 'fine_grain':
+        meta['legends_on'] = False
     common_x = g_threads
     y_serieses = []
     y_errors = []
@@ -99,7 +104,6 @@ def pack_plotting_data_aborts(processed_results, cont):
     meta['y_label'] = 'Aborts (%)'
     meta['save_name'] = '{}_{}_{}_abort.pdf'.format(exp.TYPE, exp.NAME, cont)
 
-
     return (meta, common_x, y_serieses)
 
 def draw_bars(meta_info, common_x, y_serieses, y_errors):
@@ -122,15 +126,16 @@ def draw_bars(meta_info, common_x, y_serieses, y_errors):
 
     for i in range(len(g_systems)):
         r = ax.bar(ind+width*i, y_serieses[i], width,
-            color=meta_info['plot_colors'][i],
+            color=meta_info['fill_colors'][i], edgecolor='black', hatch=meta_info['hatches'][i],
             yerr=y_errors[i], error_kw=cm.ERROR_KW)
         rects.append(r)
 
     ax.set_ylabel(meta_info['y_label'])
-    ax.set_ylim(ymin=0)
+    ax.set_ylim(ymin=0, ymax=2.0)
     ax.set_xticks(ind + width*len(g_systems)/2)
     ax.set_xticklabels(['{}'.format(t) for t in common_x])
     ax.set_xlabel(meta_info['x_label'])
+    ax.xaxis.set_ticks_position('none')
 
     if meta_info['legends_on']:
         ax.legend([r[0] for r in rects],
@@ -138,8 +143,8 @@ def draw_bars(meta_info, common_x, y_serieses, y_errors):
                   loc='best')
 
     plt.tight_layout()
-    #plt.show()
-    plt.savefig(meta_info['save_name'])
+    plt.show()
+    #plt.savefig(meta_info['save_name'])
 
 if __name__ == '__main__':
     with open(exp.RESULT_FILE, 'r') as rf:
