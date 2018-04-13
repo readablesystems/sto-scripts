ba755b6065a72ccd8a68cafb3fc4a8c77c2e9a7d
Yihe Huang <yihehuang@g.harvard.edu>
2018-04-13 12:18:54 -0400

    script tweaks

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
 # Benchmark configurations
 
 class TPCCConfig:
-    def __init__(self):
-        self.NAME = 'tpcc'
-        self.DIM1 = [8, 16, 24, 32, 64, 96, 128]
-        self.DIM2 = ['default', 'swiss', 'adaptive', '2pl', 'tictoc']
-        self.DIM3 = ['low,coarse', 'low,fine', 'high,coarse', 'high,fine']
+    NAME = 'tpcc'
+    DIM1 = [8, 16, 24, 32, 64, 96, 128]
+    DIM2 = ['default', 'swiss', 'adaptive', '2pl', 'tictoc']
+    DIM3 = ['low,coarse', 'low,fine', 'high,coarse', 'high,fine']
 
 
 class WikiConfig:
-    def __init__(self):
-        self.NAME = 'wiki'
-        self.DIM1 = [8, 16, 24, 32, 48]
-        self.DIM2 = ['coarse', 'fine']
-        self.DIM3 = ['one']
+    NAME = 'wiki'
+    DIM1 = [8, 16, 24, 32, 48]
+    DIM2 = ['coarse', 'fine']
+    DIM3 = ['one']
--- run.py
+++ run.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
 
-from runner import BenchRunner
-from runner import TPCCRunner
-from runner import WikiRunner
+from runner import BenchRunner,TPCCRunner,WikiRunner
+from config import TPCCConfig,WikiConfig
 import optparse, json, os
-import config as bc
 
-bm = {
-    'tpcc': bc.TPCCConfig(),
-    'wiki': bc.WikiConfig()
-}
-
-rm = {
-    'tpcc': TPCCRunner,
-    'wiki': WikiRunner
+brm = {
+    'tpcc': (TPCCConfig, TPCCRunner),
+    'wiki': (WikiConfig, WikiRunner)
 }
 
 
@@ -22,19 +15,26 @@ def result_file_name(name):
 
 
 def get_runner_and_file(bench_name):
-    if bench_name in bm:
-        return (rm[bench_name](bm[bench_name].NAME, bm[bench_name].DIM1, bm[bench_name].DIM2, bm[bench_name].DIM3),
-                result_file_name(bm[bench_name].NAME))
+    if bench_name in brm:
+        cnf = brm[bench_name][0]
+        rnr = brm[bench_name][1]
+        return (rnr(cnf.NAME, cnf.DIM1, cnf.DIM2, cnf.DIM3),
+                result_file_name(cnf.NAME))
     else:
         print('unknown benchmark: {}'.format(bench_name))
         exit()
 
 
 if __name__ == '__main__':
-    psr = optparse.OptionParser()
-    psr.add_option("-f", "--force-update", action="store_true", dest="force_update", default=False)
-    psr.add_option("-d", "--dry-run", action="store_true", dest="dry_run", default=False)
-    psr.add_option("-n", "--num-trails", action="store", type="int", nargs=1, dest="ntrails", default=[0])
+    usage = "Usage: %prog [options] benchmark\n\nSupported benchmarks: "
+    usage += ', '.join(brm.keys())
+    psr = optparse.OptionParser(usage=usage)
+    psr.add_option("-f", "--force-update", action="store_true", dest="force_update", default=False,
+                   help="Set if force overwriting existing result file. Default is False.")
+    psr.add_option("-d", "--dry-run", action="store_true", dest="dry_run", default=False,
+                   help="Set if want a dry run (printing out commands only). Default is False.")
+    psr.add_option("-n", "--num-trails", action="store", type="int", nargs=1, dest="ntrails", default=[0],
+                   help="Set the number of repeated runs for each experiment. Default is 5.")
 
     opts, args = psr.parse_args()
 
