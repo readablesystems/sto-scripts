839233d568a7ed48c7cfa11939da9ec0991a46ca
Yihe Huang <yihehuang@g.harvard.edu>
2018-04-13 19:42:25 -0400

    update .gitignore and add TPCCGraphConfig

--- config.py
+++ config.py
@@ -32,6 +32,22 @@ class WikiConfig:
 # Graph config
 
 
+class TPCCGraphConfig:
+    INFO = {
+        'x_label': '# threads',
+        'y_label': 'Throughput (Mtxns/sec)',
+        'series_names': ('Coarse', 'Fine'),
+        'fill_colors': ('black', 'white', 'white', 'white', 'white'),
+        'hatches': ('', '++', '///', 'xx', ''),
+        'legends_on': True
+    }
+    DIM1 = TPCCConfig.DIM1
+    DIM2 = TPCCConfig.DIM2
+    DIM3 = TPCCConfig.DIM3
+    D3TITLES = ['High contention TPC-C (8 warehouses fixed)']
+    D3FNAMES = ['tpcc_bench']
+
+
 class WikiGraphConfig:
     INFO = {
         'x_label': '# threads',
