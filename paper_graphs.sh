9991d1b191f6ac5ecc8a6a1bfd9c91e45ce804de
Yihe Huang <yihehuang@g.harvard.edu>
2019-09-27 22:57:09 -0400

    add shortcut to generate all paper graphs

---
+++ paper_graphs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./plotter.py y_base t_base w_base r_base tpcc-index-contention \
+             tpcc-stacked-factors-occ t_xsys t_semopt t_semind \
+             y_semopt w_scale_merged r_scale_merged -tpdf
