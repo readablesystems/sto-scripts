2b5608a3a8476334f3a3bfe50042f2f5bcb84dec
Yihe Huang <yihehuang@g.harvard.edu>
2019-10-16 15:21:27 -0400

    Update paper_garph.sh.

--- paper_graphs.sh
+++ paper_graphs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./plotter.py y_base t_base w_base r_base tpcc-index-contention \
-             tpcc-stacked-factors-occ t_xsys t_semopt t_semind \
+             t_noncumu_o t_xsys t_semopt t_semind \
              y_semopt w_scale_merged r_scale_merged -tpdf -n
